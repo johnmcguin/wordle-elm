f922b526b9b75257e7fcf07bba2b41c517ad9b9a
John McGuin <john@waysoftware.dev>
2024-03-08 14:54:16 -0700

    refactor: comments + file organization

--- src/Main.elm
+++ src/Main.elm
@@ -3,32 +3,61 @@ module Main exposing (main)
 import Browser
 import Html exposing (Html, div, text)
 
-type Msg = Increment | Decrement
-type alias Model = Int
+
+
+-- Main
+
 
 main : Program () Int Msg
 main =
-    Browser.element { init = init , update = update, view = view, subscriptions = subscriptions }
+    Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
 
-subscriptions : Model -> Sub Msg
-subscriptions model =
-  Sub.none
 
-init : () -> (Model, Cmd Msg)
-init _ = 
-  (0, Cmd.none)
+
+-- Model
+
+
+type alias Model =
+    Int
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( 0, Cmd.none )
+
+
+
+-- Update
+
+
+type Msg
+    = Increment
+    | Decrement
 
 
 update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
     case msg of
         Increment ->
-            (model + 1, Cmd.none)
+            ( model + 1, Cmd.none )
 
         Decrement ->
-            (model - 1, Cmd.none)
+            ( model - 1, Cmd.none )
+
+
+
+-- Subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- Views
 
 
 view : Model -> Html Msg
-view model =
-  div [] [text "hello wordle"]
+view _ =
+    div [] [ text "hello wordle" ]
