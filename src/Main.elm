8f297b110cf0ae0f79003b82826453aeb5c7fdae
John McGuin <john@waysoftware.dev>
2024-03-08 14:47:00 -0700

    refactor: update to Browser.element

--- src/Main.elm
+++ src/Main.elm
@@ -8,17 +8,25 @@ type alias Model = Int
 
 main : Program () Int Msg
 main =
-    Browser.sandbox { init = 0, update = update, view = view }
+    Browser.element { init = init , update = update, view = view, subscriptions = subscriptions }
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+init : () -> (Model, Cmd Msg)
+init _ = 
+  (0, Cmd.none)
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
     case msg of
         Increment ->
-            model + 1
+            (model + 1, Cmd.none)
 
         Decrement ->
-            model - 1
+            (model - 1, Cmd.none)
 
 
 view : Model -> Html Msg
