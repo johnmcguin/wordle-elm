711916237b421e9b740f00b7b3c796816549a539
John McGuin <john@waysoftware.dev>
2024-03-11 10:17:12 -0600

    feat(wip): notes + initial game model and game events

--- src/Main.elm
+++ src/Main.elm
@@ -9,42 +9,63 @@ import Html.Attributes as HA
 -- Main
 
 
-main : Program () Int Msg
+main : Program () Model Msg
 main =
     Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
 
 
 
 -- Model
--- nyt note - store list of guesses. store current row index
+{-
+   nyt note - store list of guesses. store current row index
+   Keyboard:
+   - needs to render a keyboard. But also needs state. Needs to track which letters in the
+   keyboard have been guessed as well as the type of guess (Unguessed, CorrectGuess, IncorrectGuessInWord, IncorrectGuess)
+   - needs to work on touch events (button) as well as keydown events (typing)
+
+   Game Board:
+   - needs to render a 6x5 board (6 guesses of 5 letter words)
+   - needs to track keypresses from the keyboard and render the letters pressed in real time for the _current_ row
+   - needs to track which row is currently being guessed
+   - needs to track the state of the letters against the puzzle word for a given guess (guessed?, incorrect in word, correct in word, not in word)
+   - needs to check if a submitted guess is in the list of viable words
+   - needs to check if a submitted guess is correct
+   - needs to track final row / guess / finish of the game
+   - needs to handle end game (show result)
+   - needs to track list of guesses (to hydrate the game board if refresh, etc)
+
+   Shared state:
+   - letters guessed and their state
+   - game (Initial, Playing, Over)
+-}
 
 
-type alias Model =
-    Int
+type Model
+    = Initial
+    | Playing
+    | Ended
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( 0, Cmd.none )
+    ( Initial, Cmd.none )
 
 
 
 -- Update
+-- List of events in the game:
+-- - KeyPressed String (is this is?)
 
 
 type Msg
-    = Increment
-    | Decrement
+    = KeyPressed String
 
 
 update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
     case msg of
-        Increment ->
-            ( model + 1, Cmd.none )
-
-        Decrement ->
-            ( model - 1, Cmd.none )
+        KeyPressed _ ->
+            ( model, Cmd.none )
 
 
 
@@ -62,8 +83,8 @@ subscriptions _ =
 -- \u{232b} - delete key
 
 
-rows : List (List Char)
-rows =
+keyboardRows : List (List Char)
+keyboardRows =
     [ [ 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p' ]
     , [ 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l' ]
     , [ '⏎', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '⌫' ]
@@ -84,7 +105,7 @@ view _ =
                     ]
                 ]
             ]
-        , div [ HA.class "keyboard" ] (List.map renderRow rows)
+        , div [ HA.class "keyboard" ] (List.map renderRow keyboardRows)
         ]
 
 
