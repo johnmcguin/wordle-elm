cd9149d6dd4021bee7dc85820bd7eea540a07fc1
John McGuin <john@waysoftware.dev>
2024-03-14 16:24:33 -0600

    docs: rm old game rules / logic comment

--- src/Main.elm
+++ src/Main.elm
@@ -18,31 +18,6 @@ main =
 
 
 -- Model
-{-
-   nyt note - store list of guesses. store current row index
-   Keyboard:
-   - needs to render a keyboard. But also needs state. Needs to track which letters in the
-   keyboard have been guessed as well as the type of guess (Unguessed, CorrectGuess, IncorrectGuessInWord, IncorrectGuess)
-   - needs to work on touch events (button) as well as keydown events (typing)
-
-   Game Board:
-   - needs to render a 6x5 board (6 guesses of 5 letter words)
-   - needs to track keypresses from the keyboard and render the letters pressed in real time for the _current_ row
-   - needs to track which row is currently being guessed
-   - needs to track the state of the letters against the puzzle word for a given guess (guessed?, incorrect in word, correct in word, not in word)
-   - needs to check if a submitted guess is in the list of viable words
-   - needs to check if a submitted guess is correct
-   - needs to track final row / guess / finish of the game
-   - needs to handle end game (show result)
-   - needs to track list of guesses (to hydrate the game board if refresh, etc)
-
-   Shared state:
-   - letters guessed and their state
-   - game (Initial, Playing, Over)
-    - if Initial, game board is empty, keyboard is empty
-    - if Playing, game board is _possibly_ filles, keyboard is _possibly_ filled
-    - if Over, game board is filled, keyboard is filled
--}
 
 
 type GameResult
@@ -62,10 +37,7 @@ init _ =
 
 
 
--- ( Initial, Cmd.none )
 -- Update
--- List of events in the game:
--- - KeyPressed String (is this is?)
 
 
 type Msg
