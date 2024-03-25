f0976e5e4828fc1a873423cca471432fa4408ddc
John McGuin <john@waysoftware.dev>
2024-03-25 10:03:48 -0600

    docs: add docs comment

--- main.js
+++ main.js
@@ -10,4 +10,13 @@ if (process.env.NODE_ENV === "development") {
 }
 
 const root = document.querySelector("#app div");
-const app = Elm.Main.init({ node: root, flags: { word: "three" } });
+Elm.Main.init({
+  node: root,
+  flags: {
+    /**
+     * update the value of the word. Optionally, remove the flag.
+     * Designed with flags to take a value from a backend eventually
+     * */
+    word: "three",
+  },
+});
